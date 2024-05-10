1937032ced91efaef9e78e140b6f44d1429f4cff
nacho83-create <167802215+G4-2024@users.noreply.github.com>
2024-05-10 16:29:05 -0300

    inicio de main
    se inicia con main y carga de fotos y formato de ofertas

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,7 @@
 function validarFormulario() {
     const nombre = document.getElementById('nombre').value;
     const correo = document.getElementById('correo').value;
+    const telefono = document.getElementById('telefono').value; 
     const mensaje = document.getElementById('mensaje').value;
 
     // Validación básica
@@ -17,6 +18,11 @@ function validarFormulario() {
         return false;
     }
 
-
+    // Validación de formato de numero telefono
+    const telefonoRegex = /^\d{10}$/;
+    if (!telefonoRegex.test(telefono)) {
+        alert("Por favor, introduce un número de teléfono válido (10 dígitos).");
+        return false; 
+    }
     return true;
 }
